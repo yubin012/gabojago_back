4d424f811a31338a2dcec1be145a298a9549ecce
yubin012 <pyb2675@naver.com>
2022-08-05 20:23:46 +0900

    지도 만드는 중입니다.

--- gabojago/urls.py
+++ gabojago/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('first/',views.first_view, name='first_view'),
 ]
